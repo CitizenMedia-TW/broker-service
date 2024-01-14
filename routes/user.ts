dc1a198d2a9a5dcf2deb2b7958658e6422442847
GNITOAHC <chaotingchen10@gmail.com>
2024-01-14 23:12:26 +0800

    Add public user info (#2)
    * refactor: New constants file
    
    * feat: Add jwt-sign type define
    
    * feat: Add public user info

--- routes/user.ts
+++ routes/user.ts
@@ -2,8 +2,19 @@ import express from 'express'
 import { User } from '@/models'
 import { jwt_protect } from './auth.utils' // Require Headers Authorization
 import jwt from 'jsonwebtoken'
-import { JWT_SECRET } from '@/index'
+import { JWT_SECRET } from '@/src/constants'
 const router = express.Router()
+
+router.get('/public-profile/:id', async (req, res) => {
+  const user = await User.findOne({ _id: req.params.id })
+  if (!user) return res.status(400).send({ error: 'User not found' })
+  return res.status(200).send({
+    name: user.username,
+    email: user.email,
+    avatar: user.avatar,
+    profileLinks: user.profileLinks,
+  })
+})
 
 interface DecodedToken {
   id: string
