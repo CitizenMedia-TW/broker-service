9f11dcd80115fc95f6329334b25ad02f6f400c78
timsu92 <33785401+timsu92@users.noreply.github.com>
2024-03-29 17:50:39 +0800

    fix(auth): mark undefined when user not found

--- src/database/get.ts
+++ src/database/get.ts
@@ -8,7 +8,7 @@ type User = {
   created_at: Date
   links: string[]
 }
-export async function getUser(mail: string): Promise<User> {
+export async function getUser(mail: string): Promise<User | undefined> {
   const qString = 'select user_t.* from user_t where mail = $1'
   const res = await query(qString, [mail])
   return res.rows[0] as User
