8e909855ee42a354a8ef9a208806ed549b285349
GINTOAHC <chaotingchen10@gmail.com>
2024-04-20 18:06:02 +0800

    feat: Add public info support

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,54 +1,46 @@
 import express from 'express'
-import { User } from '@/src/models'
 import { jwtProtect } from './auth.utils' // Require Headers Authorization
+import { getUser } from '../database/get'
+import { patchLinks } from '../database/patch'
 const router = express.Router()
 
-router.get('/public-profile/:id', async (req, res) => {
-  const user = await User.findOne({ _id: req.params.id })
+router.get('/public-profile/:mail', async (req, res) => {
+  if (!req.params.mail) return res.status(400).send({ error: 'Invalid mail' })
+  const user = await getUser(req.params.mail)
   if (!user) return res.status(400).send({ error: 'User not found' })
   return res.status(200).send({
-    name: user.username,
-    email: user.email,
+    name: user.name,
+    email: user.mail,
     avatar: user.avatar,
-    profileLinks: user.profileLinks,
+    links: user.links,
   })
 })
 
 router.get('/profile-links', jwtProtect, async (req, res) => {
-  const user = await User.findOne({ _id: req.body.decoded.id })
+  if (!req.params.mail) return res.status(400).send({ error: 'Invalid mail' })
+  const user = await getUser(req.body.decoded.mail)
   if (!user) return res.status(400).send({ error: 'User not found' })
-  return res.status(200).send({ profileLinks: user.profileLinks ?? {} })
+  return res.status(200).send({
+    links: user.links,
+  })
 })
 
-router.post('/profile-links', jwtProtect, async (req, res) => {
+router.patch('/profile-links', jwtProtect, async (req, res) => {
   /*
    * req.body = {
    *  remove: string[],
    *  add: string[]
    * }
    */
-  const user = await User.findOne({ _id: req.body.decoded.id })
-  if (!user) return res.status(400).send({ error: 'User not found' })
-
-  if (req.body.remove) {
-    req.body.remove.forEach((link: string) => {
-      const index = user.profileLinks.indexOf(link)
-      if (index == -1) return // If link doesn't exists
-      user.profileLinks.splice(index, 1)
-    })
+  const links: string[] = req.body.remove.concat(req.body.add)
+  for (let i = 0; i < links.length; i++) {
+    if (typeof links[i] !== 'string')
+      return res.status(400).send({ error: 'Invalid link' })
+    const patchRes = await patchLinks(req.body.decoded.mail, links[i])
+    if (!patchRes)
+      return res.status(500).send({ error: 'Error patching links' })
   }
-
-  if (req.body.add) {
-    req.body.add.forEach((link: string) => {
-      const index = user.profileLinks.indexOf(link)
-      if (index != -1) return // If link already exists
-      user.profileLinks.push(link)
-    })
-  }
-
-  const saved = await user.save()
-  if (!saved) return res.status(400).send({ error: 'Error saving user' })
-  return res.status(200).send({ profileLinks: user.profileLinks })
+  return res.status(200).send({ message: 'Links patched successfully' })
 })
 
 export default router
